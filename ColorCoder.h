570e78dfcad1da6f0ebcbb669b88bf0910d9017c
Selva1507 <selvakumarnamakodi@gmail.com>
2025-09-11 19:24:54 +0530

    V1

--- ColorCoder.h
+++ ColorCoder.h
@@ -1,4 +1,28 @@
+#include <string>
+
 namespace TelCoColorCoder
 {
-   
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    extern const char* MajorColorNames[];
+    extern const char* MinorColorNames[];
+    extern int numberOfMajorColors;
+    extern int numberOfMinorColors;
+
+    class ColorPair {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor);
+        MajorColor getMajor();
+        MinorColor getMinor();
+        std::string ToString();
+    };
+
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+
+    void PrintColorManual();
 }
